54e448ecae266b407776d9bdaa5aed5faad4af45
Michael Gouveia <mtgouveia06@gmail.com>
2025-01-14 13:30:48 -0500

    Yeah

--- Gouveia-Michael-Assignment1/Program.cs
+++ Gouveia-Michael-Assignment1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Welcome to the Game of Choice! Choose your fate through a dangerouse and amazing journey.");
+Console.WriteLine("Picture the scene! It is ancient times in a fantasy world and you have been captured by a band of mercenaries." +
+    "Decide which character you are interested in playing as!");
+
+Console.WriteLine("Type [J] to play as John Souls, the Legend that has never lost a duel!");
+Console.WriteLine("Type [C] to play as Chuck McGill! Born from a human father and fish mother, Chuck can swim any body of water and not have to worry about air!");
+Console.WriteLine("Type [B] to play as Bartollo Cubbins! Wicked slight of hand, can use close range magic and fool his enemies!");
+Console.WriteLine("Type [K] to use the female lead Kora The Bear! She is human but gets the nickname Bearalina because of her immense strenght!");
+
