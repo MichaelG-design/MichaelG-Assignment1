eaf3130815bef60b825276a02077f537adcbdeb3
Michael Gouveia <mtgouveia06@gmail.com>
2025-01-14 17:32:11 -0500

    again

--- Gouveia-Michael-Assignment1/Program.cs
+++ Gouveia-Michael-Assignment1/Program.cs
@@ -1,10 +1,27 @@
 ﻿
-Console.WriteLine("Welcome to the Game of Choice! Choose your fate through a dangerouse and amazing journey.");
-Console.WriteLine("Picture the scene! It is ancient times in a fantasy world and you have been captured by a band of mercenaries." +
-    "Decide which character you are interested in playing as!");
+Console.WriteLine("You are in a match of hand to hand combat with MILLIONS watching you, will you make the right choice and enjoy glory! Or will you suffer the shame of defeat?");
+Console.WriteLine("DING DING DING DING");
+Console.WriteLine("The fight begins, your oponent makes the first move and hones in on you....");
 
-Console.WriteLine("Type [J] to play as John Souls, the Legend that has never lost a duel!");
-Console.WriteLine("Type [C] to play as Chuck McGill! Born from a human father and fish mother, Chuck can swim any body of water and not have to worry about air!");
-Console.WriteLine("Type [B] to play as Bartollo Cubbins! Wicked slight of hand, can use close range magic and fool his enemies!");
-Console.WriteLine("Type [K] to use the female lead Kora The Bear! She is human but gets the nickname Bearalina because of her immense strenght!");
-
+Console.WriteLine("Your oponent moves his left fist as to hit you but then quickly goes for a right-hook!" +
+    "What will you do? Type either [block] or [punch] or [weave]");
+string choice1 = Console.ReadLine();
+if (choice1 == "block")
+{
+    Console.WriteLine("You successfully blocked!");
+    Console.WriteLine("He quickly goes for a punch in the gut using his left, type either [tankit] or [dodge] or [grab]");
+    string choice2 = Console.ReadLine();
+    if (choice2 == "tankit")
+    {
+        Console.WriteLine("You took the hit, and with a sudden urge of adrenaline you land a one-two!");
+        Console.WriteLine("Oh no! Your oponent counters your second punch and rushes for a hit back! What do you do? [parry] [block] [dodge]");
+    }
+}
+if (choice1 == "punch")
+{
+    Console.WriteLine(
+}
+if (choice1 == "weave")
+{
+    Console.WriteLine(
+}
